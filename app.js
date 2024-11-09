560ebef9a4a49dd9b8c47984e49d3015d67c857b
Eymen Demez <136674304+WtmcX@users.noreply.github.com>
2024-11-09 08:02:56 +0300

    Fix the turkish comments

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ function checkClient() {
     const clientSelect = document.getElementById('clientSelect');
     const resolutionSelect = document.getElementById('resolutionSelect');
 
-    // Eğer Vanilla seçildiyse çözünürlük menüsünü kitleriz
     if (clientSelect.value === 'Vanilla') {
         resolutionSelect.classList.add('disabled-select');
         resolutionSelect.classList.remove('enabled-select');
-        resolutionSelect.disabled = true; // Menüyü devre dışı bırak
+        resolutionSelect.disabled = true; 
     } else {
         resolutionSelect.classList.add('enabled-select');
         resolutionSelect.classList.remove('disabled-select');
-        resolutionSelect.disabled = false; // Menüyü tekrar aktif et
+        resolutionSelect.disabled = false;
     }
 }
 
@@ -26,7 +25,6 @@ function launchClient() {
         return;
     }
 
-    // Vanilla seçiliyse varsayılan bir çözünürlük ekleyin (örneğin, 1920x1080)
     let pageUrl;
     if (selectedClient === 'Vanilla') {
         pageUrl = `Vanilla.html`;
@@ -34,6 +32,6 @@ function launchClient() {
         pageUrl = `${selectedClient}${selectedResolution}.html`;
     }
 
-    // Yeni sekmede aç
+
     window.open(pageUrl, '_blank');
 }
